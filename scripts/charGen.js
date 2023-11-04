02ad43ec9f43e633d2eaa02102f0916f242cda8b
Michael Medina <rhotick@gmail.com>
2023-11-04 17:56:10 -0400

    Update charGen.js

--- scripts/charGen.js
+++ scripts/charGen.js
@@ -5,29 +5,30 @@ var data = data || {};
 
 (function(module){
   module.Character = function() {
-    this.hairColor = app.randomItem(data.hairColors);
-    this.hairStyle = app.randomItem(data.hairStyles);
-    this.eyeColor = app.randomItem(data.eyeColors);
-    this.faceShape = app.randomItem(data.faceShapes);
-    this.race = app.randomItem(data.races);
-    this.gender = app.randomItem(data.genders);
-    this.feature = app.randomItem(data.features);
-    this.height =
-      Math.floor(
-        Math.random()
-          * (this.race.maxHeight[this.gender]-this.race.minHeight[this.gender])
-          + this.race.minHeight[this.gender]
-      );
-    this.weight =
-      app.calcWeight(
-        this.height,
-        Math.floor(Math.random() * (this.race.maxBmi - this.race.minBmi) + this.race.minBmi)
-      );
-    this.orientation = app.randomItem(data.orientations);
-    this.attitude = app.randomItem(data.attitudes);
-    this.background = app.randomItem(data.backgrounds);
-    this.hobby = app.randomItem(data.hobbies);
-    this.traits = [app.randomItem(data.traits), app.randomItem(data.traits)];
-    this.motivation = app.randomItem(data.motivations);
+    this.physique = app.randomItem(data.physique);
+    this.face = app.randomItem(data.face);
+    this.speech = app.randomItem(data.speech);
+    this.virtue = app.randomItem(data.virtue);
+    this.flaw = app.randomItem(data.flaw);
+    this.misfortunes = app.randomItem(data.misfortunes);
+    this.trauma = app.randomItem(data.trauma);
+    this.beliefs = app.randomItem(data.beliefs);
+    this.social = app.randomItem(data.social);
+    this.startingMomento = app.randomItem(data.startingMomento);
+    this.startingBackground = app.randomItem(data.startingBackground);
+    this.startingWeapon = app.randomItem(data.startingWeapon);
+    this.startingGear = app.randomItem(data.startingGear);
+    this.startingBonus = app.randomItem(data.startingBonus);
+    // this.height =
+    //   Math.floor(
+    //     Math.random()
+    //       * (this.race.maxHeight[this.gender]-this.race.minHeight[this.gender])
+    //       + this.race.minHeight[this.gender]
+    //   );
+    // this.weight =
+    //   app.calcWeight(
+    //     this.height,
+    //     Math.floor(Math.random() * (this.race.maxBmi - this.race.minBmi) + this.race.minBmi)
+    //   );
   };
 })(app);
