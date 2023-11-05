0a040170adb3144f459694cb4b1e043b4e7a8909
Michael Medina <rhotick@gmail.com>
2023-11-04 20:43:38 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -28,7 +28,7 @@ var app = app || {};
     for (var i = 0; i < n; i++)
         a[i] = Math.floor(Math.random() * 6) + 1;
     return a[0] + a[1] + a[2];
-}
+};
 
   module.main = function() {
     let character = new app.Character();
