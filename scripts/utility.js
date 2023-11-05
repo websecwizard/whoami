e1f48bc68fffc85e82e1746f355b2ca2d719e12f
Michael Medina <rhotick@gmail.com>
2023-11-04 20:22:57 -0400

    Update utility.js

--- scripts/utility.js
+++ scripts/utility.js
@@ -6,4 +6,10 @@ var app = app || {};
     let randomIndex = Math.floor(Math.random() * Math.floor(array.length));
     return array[randomIndex];
   };
+    module.roll = function(n) {
+    var a = Array(n);
+    for (var i = 0; i < n; i++)
+        a[i] = Math.floor(Math.random() * 6) + 1;
+    return a[0] + a[1] + a[2];
+};
 })(app);
